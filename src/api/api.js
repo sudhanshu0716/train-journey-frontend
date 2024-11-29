a65eca7ebc8c7913acd0355881be17890bc45f14
sudhanshu0716 <122021740+sudhanshu0716@users.noreply.github.com>
2024-11-30 01:29:36 +0530

    commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = "http://localhost:5000/api/reports"; 
+const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:5000/api/reports";
 
 export const checkReports = async (trainNo, date) => {
   try {
