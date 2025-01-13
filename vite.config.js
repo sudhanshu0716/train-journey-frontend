d81ad9eaa0d954ba66ca588f5ccdb72029a3e1de
satyam rai <satyamrai200311@gmail.com>
2025-01-14 00:37:09 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+    build: {
+    outDir: 'build', // Set the output directory to 'build'
+    chunkSizeWarningLimit: 5000, // Adjust chunk size limit if necessary (default is 500)
+  },
 })
