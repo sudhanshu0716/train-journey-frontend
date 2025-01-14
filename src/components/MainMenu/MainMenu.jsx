4e380638d37b4c0ab184ecbdb0609371f213bf67
sudhanshu0716 <122021740+sudhanshu0716@users.noreply.github.com>
2025-01-14 23:22:50 +0530

    Update MainMenu(added navbar)

--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -1,9 +1,6 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./MainMenu.css";
-import scenicImg from "../../assets/scenic.jpg";
-import lostFoundImg from "../../assets/lf.png";
-import crushTheRushImg from "../../assets/crush_the_rush.jpg";
 
 // Grid images
 import image1 from "../../assets/1.jpg";
@@ -22,44 +19,62 @@ const images = [image1, image2, image3, image4, image5, image6];
 const MainMenu = () => {
   return (
     <div className="main-menu-wrapper">
-
-      <div className="main-menu-container">
-        <h1 className="main-menu-title">MAIN MENU</h1>
-
-        <div className="main-menu-cards">
-          <div className="main-menu-card">
+      <nav className="horizontal-nav">
+        <ul>
+          <li className="nav-item">
             <Link to="/scenic">
-              <div className="main-card-image">
-                <img src={scenicImg} alt="Scenic Views" />
-              </div>
-              <div className="main-card-title">Scenic Views</div>
+              Scenic Views
+              <span className="underline"></span>
             </Link>
-          </div>
-
-          <div className="main-menu-card">
-            <Link to="/lost">
-              <div className="main-card-image">
-                <img src={lostFoundImg} alt="Lost and Found" />
-              </div>
-              <div className="main-card-title">Lost and Found</div>
-            </Link>
-          </div>
-
-          <div className="main-menu-card">
+          </li>
+          <li className="nav-item">
             <Link to="/crush">
-              <div className="main-card-image">
-                <img src={crushTheRushImg} alt="Crush the Rush" />
-              </div>
-              <div className="main-card-title">Crush the Rush</div>
+              Crush the Rush
+              <span className="underline"></span>
             </Link>
-          </div>
-        </div>
-      </div>
-        <hr className="mmhr"/>
-        <hr className="mmhr"/>
-      <h1 className="heading-grid">Most Beautiful Train Journeys In India You Must Experience</h1>
+          </li>
+          <li className="nav-item">
+            <Link to="/lost">
+              Lost and Found
+              <span className="underline"></span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/instant-help">
+              Instant Help
+              <span className="underline"></span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/privacy-and-policies">
+              Privacy Policy
+              <span className="underline"></span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/community-section">
+              Community 
+              <span className="underline"></span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/about">
+              About
+              <span className="underline"></span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
 
-  
+      <hr className="mmhr" />
+      <hr className="mmhr" />
+      <br />
+      <br />
+      <br />
+      <h1 className="heading-grid">
+        Most Beautiful Train Journeys In India You Must Experience
+      </h1>
+
       <div className="image-grid-container">
         {images.map((image, index) => (
           <div key={index} className="grid-item">
@@ -67,13 +82,31 @@ const MainMenu = () => {
           </div>
         ))}
       </div>
+
       <div className="mmfoot">
         <h1 className="mmfoot-head">CONTACT US</h1>
         <div className="mmfoot-list">
-          <a href="https://www.instagram.com/travel._.trains?igsh=dXd5eDgyajB0MG82" className="mmfoot-list-item"><FaInstagram /></a>
-          <a href="https://www.linkedin.com/in/sudh0716/" className="mmfoot-list-item"><FaLinkedin /></a>
-          <a href="" className="mmfoot-list-item"><BiLogoGmail /></a>
-          <a href="https://github.com/sudhanshu0716" className="mmfoot-list-item"><FaGithub /></a>
+          <a
+            href="https://www.instagram.com/travel._.trains?igsh=dXd5eDgyajB0MG82"
+            className="mmfoot-list-item"
+          >
+            <FaInstagram />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/sudh0716/"
+            className="mmfoot-list-item"
+          >
+            <FaLinkedin />
+          </a>
+          <a href="" className="mmfoot-list-item">
+            <BiLogoGmail />
+          </a>
+          <a
+            href="https://github.com/sudhanshu0716"
+            className="mmfoot-list-item"
+          >
+            <FaGithub />
+          </a>
         </div>
         <h4 className="mmcopyr">2024 © SUDHANSHU, All Rights Reserved</h4>
       </div>
