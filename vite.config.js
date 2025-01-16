571eb6b9ace79c2f560d52fb66fc5f469e2940c2
sudhanshu0716 <122021740+sudhanshu0716@users.noreply.github.com>
2025-01-16 08:51:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
     build: {
-    outDir: 'dist', // Set the output directory to 'build'
     chunkSizeWarningLimit: 5000, // Adjust chunk size limit if necessary (default is 500)
   },
 })
