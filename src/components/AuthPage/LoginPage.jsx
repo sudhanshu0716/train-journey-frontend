a15b39d2e749106d6d3f7abbf3ebb0e6d2b25474
Satyam <raisatyam710@gmail.com>
2025-01-20 16:21:36 +0530

    login signup with loader

--- src/components/AuthPage/LoginPage.jsx
+++ src/components/AuthPage/LoginPage.jsx
@@ -2,14 +2,19 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import "./AuthPage.css";
+import Login from "../../assets/login.jpg";
+import { ThreeDots } from 'react-loader-spinner'; // Import the ThreeDots loader
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false); // Add state for loading
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
+    setIsLoading(true); // Set loading state to true before fetch
+
     try {
       const response = await fetch("https://train-journey-backend.onrender.com/api/auth/login", {
         method: "POST",
@@ -22,53 +27,67 @@ const LoginPage = () => {
       const data = await response.json();
 
       if (response.ok) {
-        localStorage.setItem("token", data.token); 
-        navigate("/main-menu"); 
+        localStorage.setItem("token", data.token);
+        navigate("/main-menu");
       } else {
         alert(data.message || "Login Failed");
       }
     } catch (error) {
       console.error(error);
       alert("An error occurred during login.");
+    } finally {
+      setIsLoading(false); // Set loading state to false after fetch completes
     }
   };
 
   return (
-    <div className="form-wrapper">
-      <motion.div
-        className="login-form-container"
-        initial={{ opacity: 0, y: -50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-      >
-        <h2 className="login-title bg">RailVista</h2>
-        <form onSubmit={handleLogin}>
-          <div className="form-entry">
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="Email"
-              required
-            />
-          </div>
-          <div className="form-entry">
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Password"
-              required
-            />
-          </div>
-          <button className="login-button" type="submit">
-            Login
-          </button>
-          <p>
-            Don't have an account? <Link to="/signup">Sign up</Link>
-          </p>
-        </form>
-      </motion.div>
+    <div className="login-container">
+      <div className="login-image-container">
+        {/* Add an image here using a background image or an img tag */}
+        <img src={Login} alt="Welcome to RailVista" />
+      </div>
+      <div className="login-form-container">
+        <motion.div
+          className="login-form"
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          {isLoading ? (
+            <ThreeDots color="#00BFFF" height={80} width={80} />
+          ) : (
+            <>
+              <h2 className="login-title">Namaste 🙏</h2>
+              <form onSubmit={handleLogin}>
+                <div className="form-entry">
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Email"
+                    required
+                  />
+                </div>
+                <div className="form-entry">
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password"
+                    required
+                  />
+                </div>
+                <button className="login-button" type="submit">
+                  Login
+                </button>
+                <p>
+                  Don't have an account? <Link to="/signup">Sign up</Link>
+                </p>
+              </form>
+            </>
+          )}
+        </motion.div>
+      </div>
     </div>
   );
 };
--- src/components/AuthPage/SignUpPage.jsx
+++ src/components/AuthPage/SignUpPage.jsx
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
-import { motion } from "framer-motion";
-import { Link, useNavigate } from "react-router-dom";
-import "./AuthPage.css";
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+import { Link, useNavigate } from 'react-router-dom';
+import './AuthPage.css';
+import {ThreeDots } from 'react-loader-spinner';  // Import the loader component
+import Signup from "../../assets/signup.jpg";
+
 
 const SignUpPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false); 
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true); 
+
     try {
       const response = await fetch("https://train-journey-backend.onrender.com/api/auth/signup", {
         method: "POST",
@@ -23,58 +29,72 @@ const SignUpPage = () => {
 
       if (response.ok) {
         alert("Sign-Up Successful");
-        localStorage.setItem("token", data.token);
+        // localStorage.setItem("token", data.token); // Consider using a more secure storage solution like a dedicated state management library
         navigate("/login"); 
       } else {
         console.log(data);
         alert(data.message || "Sign-Up Failed");
       }
+      setIsLoading(false); 
     } catch (error) {
       console.error(error);
       alert("An error occurred during sign-up.");
+      setIsLoading(false); 
     }
   };
 
   return (
-    <div className="form-wrapper">
-      <motion.div
-        className="login-form-container"
-        initial={{ opacity: 0, y: -50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-      >
-        <h2 className="login-title bg">Sign Up</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="form-entry">
-            <input
-              className="login-input"
-              type="email"
-              id="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-entry">
-            <input
-              className="login-input"
-              type="password"
-              id="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <button className="login-button" type="submit">
-            Sign Up
-          </button>
-          <p>
-            Already have an account? <Link to="/login">Login</Link>
-          </p>
-        </form>
-      </motion.div>
+    <div className="login-container">
+      <div className="login-image-container">
+        {/* Add an image here using a background image or an img tag */}
+        <img src={Signup} alt="Welcome to RailVista" /> 
+      </div>
+      <div className="login-form-container">
+        <motion.div 
+          className="login-form" 
+          initial={{ opacity: 0, y: -50 }} 
+          animate={{ opacity: 1, y: 0 }} 
+          transition={{ duration: 0.5 }}
+        >
+          {isLoading ? ( 
+            <ThreeDots type="ThreeDots" color="#00BFFF" height={80} width={80} /> 
+          ) : ( 
+            <> 
+              <h2 className="login-title">Sign Up</h2>
+              <form onSubmit={handleSubmit}>
+                <div className="form-entry">
+                  <input
+                    className="login-input"
+                    type="email"
+                    id="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="form-entry">
+                  <input
+                    className="login-input"
+                    type="password"
+                    id="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </div>
+                <button className="login-button" type="submit">
+                  Sign Up
+                </button>
+                <p>
+                  Already have an account? <Link to="/login">Login</Link>
+                </p>
+              </form>
+            </> 
+          )}
+        </motion.div>
+      </div>
     </div>
   );
 };
